0147a3956c5d54809442abb6bd079bd941ed9cc3
Armani Appolon <ArmaniAppolon@gmail.com>
2022-07-04 21:59:09 -0400

    first commit

--- README.md
+++ README.md
@@ -1,24 +1,29 @@
 # moz-todo-vue
 
 ## Project setup
-```
-npm install
-```
+
+  ```node
+  npm install
+  ```
 
 ### Compiles and hot-reloads for development
-```
+
+```node
 npm run serve
 ```
 
 ### Compiles and minifies for production
-```
+
+```node
 npm run build
 ```
 
 ### Lints and fixes files
-```
+
+```node
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <form @submit.prevent="onSubmit">
+      <h2 class="label-wrapper">
+        <label for="new-todo-input" class="label__lg">
+          What else you need "To Do"?
+        </label>
+      </h2>
+    <input
+      type="text"
+      id="new-todo-input"
+      name="new-todo"
+      autocomplete="off"
+      v-model.lazy.trim="label"
+    />
+    <button type="submit" class="btn btn__primary btn__lg">+Add</button>
+  </form>
+</template>
+
+<script>
+  export default {
+   methods: {
+    onSubmit() {
+      if(this.label === "") {
+        return;
+      }
+      this.$emit('todo-added', this.label);
+      console.log('Label value: ', this.label);
+      this.label = "";
+    }
+   },
+   data(){
+    return {
+      label: ""
+    }
+   }
+};
+</script>
